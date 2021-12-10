63f0f5306245a5320ca1e2e81667e588d6eabe0e
BogdanTibulca <bogdantibulca@yahoo.com>
2021-12-10 17:36:32 +0200

    Add project files.

---
+++ Tibulca_Bogdan_Lab10/Models/ShopList.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+using System;
+
+namespace Tibulca_Bogdan_Lab10.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
